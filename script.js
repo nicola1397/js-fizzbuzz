5ab8dc792493c000c5f00bd6865cd6bfb9d9c833
nicola1397 <nicola1397@outlook.it>
2024-01-11 16:05:34 +0100

    feat: added html print and colored boxes

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+const container = document.getElementById("container");
+
 for (let i = 1; i <= 100; i++) {
+  const box = document.createElement(`div`);
+  box.classList.add(`box`);
+
+  let htmlValue = i;
   if (i % 15 == 0) {
-    console.log("FizzBuzz");
+    // console.log("FizzBuzz");
+    htmlValue = "FizzBuzz";
+    box.classList.add(htmlValue);
   } else if (i % 5 == 0) {
-    console.log("Buzz");
+    // console.log("Buzz");
+    htmlValue = "Buzz";
+    box.classList.add(htmlValue);
   } else if (i % 3 == 0) {
-    console.log("Fizz");
+    // console.log("Fizz");
+    htmlValue = "Fizz";
+    box.classList.add(htmlValue);
   } else {
-    console.log(i);
+    // console.log(i);
+    htmlValue = i;
   }
+  console.log(htmlValue);
+  // box.innerHTML = `<div>${htmlValue}</div>`;
+  box.innerHTML = `<div class="col"><div class="text-center" id="color">${htmlValue}</div></div>`;
+  container.append(box);
 }
