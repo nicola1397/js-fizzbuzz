34f4cde39c4bacf584795004a9a3d032b6564aa0
nicola1397 <nicola1397@outlook.it>
2024-01-11 16:07:23 +0100

    refactor: cleaned up code

--- script.js
+++ script.js
@@ -6,23 +6,19 @@ for (let i = 1; i <= 100; i++) {
 
   let htmlValue = i;
   if (i % 15 == 0) {
-    // console.log("FizzBuzz");
     htmlValue = "FizzBuzz";
     box.classList.add(htmlValue);
   } else if (i % 5 == 0) {
-    // console.log("Buzz");
     htmlValue = "Buzz";
     box.classList.add(htmlValue);
   } else if (i % 3 == 0) {
-    // console.log("Fizz");
     htmlValue = "Fizz";
     box.classList.add(htmlValue);
   } else {
-    // console.log(i);
     htmlValue = i;
   }
+
   console.log(htmlValue);
-  // box.innerHTML = `<div>${htmlValue}</div>`;
   box.innerHTML = `<div class="col"><div class="text-center" id="color">${htmlValue}</div></div>`;
   container.append(box);
 }
